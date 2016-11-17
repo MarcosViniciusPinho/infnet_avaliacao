23bd793e3dccd246ba1af7e67f15592a638a4537
MarcosViniciusPinho <marcosjava2008@gmail.com>
2016-11-16 23:01:26 -0200

    Correção de BUG em alterar Usuário.

--- src/main/resources/static/js/cadastro/usuario/form.js
+++ src/main/resources/static/js/cadastro/usuario/form.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
     var id = $("#id").val();
-    if(id !== null && id !== ""){
+    campoSenhaDinamica(id);
+    tornarVisivelTitulo(id);
+});
+
+function campoSenhaDinamica(id){
+    if(id !== ""){
         $(".blocoEditar").attr("style", "display: none");
     }
-});
+}
+
+function tornarVisivelTitulo(id) {
+    if(id !== ""){
+        $(".cadastrar").hide();
+        $(".alterar").show();
+    } else{
+        $(".cadastrar").show();
+        $(".alterar").hide();
+    }
+}
