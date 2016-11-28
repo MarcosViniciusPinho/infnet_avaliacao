32640d1c2ff8bf411b7637ebb7eb752b36e0bcf3
MarcosViniciusPinho <marcosjava2008@gmail.com>
2016-11-28 20:13:37 -0200

    Inserindo internacionalização no sistema.

--- src/main/java/com/infnet/avaliacao/AvaliacaoApplication.java
+++ src/main/java/com/infnet/avaliacao/AvaliacaoApplication.java
@@ -25,7 +25,7 @@ public class AvaliacaoApplication extends WebMvcConfigurerAdapter {
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.US);
+		slr.setDefaultLocale(Locale.getDefault());
 		return slr;
 	}
 
