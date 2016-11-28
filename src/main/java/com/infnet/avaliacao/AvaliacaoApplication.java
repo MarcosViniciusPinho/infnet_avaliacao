67f05fcf47763a0152a3655c3a5cb84c727a4e28
MarcosViniciusPinho <marcosjava2008@gmail.com>
2016-11-28 19:42:18 -0200

    Implementando internacionalização do sistema.

--- src/main/java/com/infnet/avaliacao/AvaliacaoApplication.java
+++ src/main/java/com/infnet/avaliacao/AvaliacaoApplication.java
@@ -1,12 +1,43 @@
 package com.infnet.avaliacao;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-@SpringBootApplication
-public class AvaliacaoApplication {
+import java.util.Locale;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class AvaliacaoApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvaliacaoApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US);
+		return slr;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 }
