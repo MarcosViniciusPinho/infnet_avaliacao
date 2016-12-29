e381c98aa2e3be943d1036383f608bb6b7ce3c20
MarcosViniciusPinho <marcosjava2008@gmail.com>
2016-12-29 21:38:20 -0200

    Correção de BUG na validação obrigatória na seleção de um tópico para uma determinada avaliação.

--- src/main/resources/static/js/template/avaliacao/form.js
+++ src/main/resources/static/js/template/avaliacao/form.js
@@ -1,7 +1,16 @@
 function selecionarTopico(){
     var checkeds = new Array();
+    addConteudoTopicoDaLista(checkeds);
+    removeConteudoTopicoDaLista(checkeds);
+}
+function addConteudoTopicoDaLista(checkeds) {
     $("input[name='checks[]']:checked").each(function (){
-        checkeds.push( $(this).val());
+        checkeds.push($(this).val());
         $('#idsTemplateTopicoSelecionados').val(checkeds);
     });
+}
+function removeConteudoTopicoDaLista(checkeds){
+    if(checkeds.length === 0){
+        $('#idsTemplateTopicoSelecionados').val(checkeds);
+    }
 }
