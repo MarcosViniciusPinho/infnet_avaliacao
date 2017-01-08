3e65b387d5b77cee5d9881fc3a8c0326ac43e025
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-01-08 08:57:37 -0200

    Ajuste nos mapeamentos para inicio da implementação de Montar Avaliação - Pergunta.

--- src/main/java/com/infnet/avaliacao/entity/TemplateAvaliacaoTopicoPergunta.java
+++ src/main/java/com/infnet/avaliacao/entity/TemplateAvaliacaoTopicoPergunta.java
@@ -3,23 +3,42 @@ package com.infnet.avaliacao.entity;
 import javax.persistence.*;
 import java.io.Serializable;
 
+/**
+ * Foi necessário criar esta classe para poder mapear esta entidade associativa pois para a mesma existem 3 atributos, portanto o hibernate
+ * não gerencia esses atributos pela anotação @ManyToMany. Devido a este fator foi necessário fazer o relacionamento NxN manualmente.
+ * Obs: O atributo id só existe pois esta classe associativa está mapeada, pois se estivesse sido gerenciado pelo hibernate pela anotação
+ * dito a cima este atributo não existiria.
+ */
 @Entity
 @Table(name = "template_avaliacao_topico_pergunta")
 public class TemplateAvaliacaoTopicoPergunta implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     @OneToOne
     @JoinColumn(name = "id_template_avaliacao", nullable = false)
     private TemplateAvaliacao templateAvaliacao;
 
-    @OneToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_template_topico", nullable = false)
     private TemplateTopico templateTopico;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_template_pergunta", nullable = false)
     private TemplatePergunta templatePergunta;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public TemplateAvaliacao getTemplateAvaliacao() {
         return templateAvaliacao;
--- src/main/resources/db/migration/V08__criando_tabela_template_avaliacao_topico_pergunta.sql
+++ src/main/resources/db/migration/V08__criando_tabela_template_avaliacao_topico_pergunta.sql
@@ -1,11 +1,13 @@
 CREATE TABLE template_avaliacao_topico_pergunta(
+  id BIGINT NOT NULL,
   id_template_topico BIGINT NOT NULL,
   id_template_avaliacao BIGINT NOT NULL,
   id_template_pergunta BIGINT NOT NULL
 );
+ALTER TABLE template_avaliacao_topico_pergunta ADD CONSTRAINT pk_id_template_avaliacao_topico_pergunta PRIMARY KEY (id);
 
-ALTER TABLE template_avaliacao_topico_pergunta ADD CONSTRAINT fk_teat_id_template_topico2 FOREIGN KEY (id_template_topico) REFERENCES template_topico (id);
+ALTER TABLE template_avaliacao_topico_pergunta ADD CONSTRAINT fk_tatp_id_template_topico FOREIGN KEY (id_template_topico) REFERENCES template_topico (id);
 
-ALTER TABLE template_avaliacao_topico_pergunta ADD CONSTRAINT fk_teat_id_template_avaliacao2 FOREIGN KEY (id_template_avaliacao) REFERENCES template_avaliacao (id);
+ALTER TABLE template_avaliacao_topico_pergunta ADD CONSTRAINT fk_tatp_id_template_avaliacao FOREIGN KEY (id_template_avaliacao) REFERENCES template_avaliacao (id);
 
-ALTER TABLE template_avaliacao_topico_pergunta ADD CONSTRAINT fk_teat_id_template_pergunta FOREIGN KEY (id_template_pergunta) REFERENCES template_pergunta (id);
+ALTER TABLE template_avaliacao_topico_pergunta ADD CONSTRAINT fk_tatp_id_template_pergunta FOREIGN KEY (id_template_pergunta) REFERENCES template_pergunta (id);
