98d465197b11bcc291819c2dd598135b3e5e1553
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-01-11 00:14:43 -0200

    Alteração no script para atender a funcionalidade de Montar Avaliação - Pergunta.

--- src/main/resources/db/migration/V08__criando_tabela_template_avaliacao_topico_pergunta.sql
+++ src/main/resources/db/migration/V08__criando_tabela_template_avaliacao_topico_pergunta.sql
@@ -2,7 +2,8 @@ CREATE TABLE template_avaliacao_topico_pergunta(
   id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   id_template_topico BIGINT NOT NULL,
   id_template_avaliacao BIGINT NOT NULL,
-  id_template_pergunta BIGINT NOT NULL
+  id_template_pergunta BIGINT NOT NULL,
+  ativo INTEGER NOT NULL DEFAULT 1
 );
 
 ALTER TABLE template_avaliacao_topico_pergunta ADD CONSTRAINT fk_tatp_id_template_topico FOREIGN KEY (id_template_topico) REFERENCES template_topico (id);
--- src/main/resources/db/migration/V20__inserindo_template_avaliacao_topico_pergunta.sql
+++ src/main/resources/db/migration/V20__inserindo_template_avaliacao_topico_pergunta.sql
@@ -1,45 +1,45 @@
-insert into template_avaliacao_topico_pergunta(id, id_template_topico , id_template_avaliacao, id_template_pergunta)
-select 1, 1, 1, 1 union all
-select 2, 1, 1, 2 union all
-select 3, 1, 1, 3 union all
-select 4, 1, 1, 4 union all
-select 5, 1, 1, 5 union all
-select 6, 1, 1, 6 union all
+insert into template_avaliacao_topico_pergunta(id_template_topico , id_template_avaliacao, id_template_pergunta)
+select 1, 1, 1 union all
+select 1, 1, 2 union all
+select 1, 1, 3 union all
+select 1, 1, 4 union all
+select 1, 1, 5 union all
+select 1, 1, 6 union all
 
-select 7, 2, 1, 7 union all
-select 8, 2, 1, 8 union all
-select 9, 2, 1, 9 union all
-select 10, 2, 1, 10 union all
-select 11, 2, 1, 11 union all
-select 12, 2, 1, 12 union all
+select 2, 1, 7 union all
+select 2, 1, 8 union all
+select 2, 1, 9 union all
+select 2, 1, 10 union all
+select 2, 1, 11 union all
+select 2, 1, 12 union all
 
-select 13, 3, 1, 13 union all
-select 14, 3, 1, 14 union all
-select 15, 3, 1, 15 union all
-select 16, 3, 1, 16 union all
-select 17, 3, 1, 17 union all
+select 3, 1, 13 union all
+select 3, 1, 14 union all
+select 3, 1, 15 union all
+select 3, 1, 16 union all
+select 3, 1, 17 union all
 
-select 18, 4, 1, 18 union all
+select 4, 1, 18 union all
 
 
-select 19, 1, 2, 1 union all
-select 20, 1, 2, 2 union all
-select 21, 1, 2, 3 union all
-select 22, 1, 2, 4 union all
-select 23, 1, 2, 5 union all
-select 24, 1, 2, 6 union all
+select 1, 2, 1 union all
+select 1, 2, 2 union all
+select 1, 2, 3 union all
+select 1, 2, 4 union all
+select 1, 2, 5 union all
+select 1, 2, 6 union all
 
-select 25, 2, 2, 7 union all
-select 26, 2, 2, 8 union all
-select 27, 2, 2, 9 union all
-select 28, 2, 2, 10 union all
-select 29, 2, 2, 11 union all
-select 30, 2, 2, 12 union all
+select 2, 2, 7 union all
+select 2, 2, 8 union all
+select 2, 2, 9 union all
+select 2, 2, 10 union all
+select 2, 2, 11 union all
+select 2, 2, 12 union all
 
-select 31, 3, 2, 13 union all
-select 32, 3, 2, 14 union all
-select 33, 3, 2, 15 union all
-select 34, 3, 2, 16 union all
-select 35, 3, 2, 17 union all
+select 3, 2, 13 union all
+select 3, 2, 14 union all
+select 3, 2, 15 union all
+select 3, 2, 16 union all
+select 3, 2, 17 union all
 
-select 36, 4, 2, 18;
+select 4, 2, 18;
