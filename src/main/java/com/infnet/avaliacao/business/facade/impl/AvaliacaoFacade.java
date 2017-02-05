ea5e85ae191a0ada2dc8860e4e2387b7ca965413
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-02-05 00:36:21 -0200

    Busca do template avaliação para a avaliação em si.

--- src/main/java/com/infnet/avaliacao/business/facade/impl/AvaliacaoFacade.java
+++ src/main/java/com/infnet/avaliacao/business/facade/impl/AvaliacaoFacade.java
@@ -3,9 +3,11 @@ package com.infnet.avaliacao.business.facade.impl;
 import com.infnet.avaliacao.business.facade.IAvaliacaoFacade;
 import com.infnet.avaliacao.business.service.IAlunoService;
 import com.infnet.avaliacao.business.service.IAvaliacaoService;
+import com.infnet.avaliacao.business.service.ITemplateAvaliacaoService;
 import com.infnet.avaliacao.business.service.ITurmaService;
 import com.infnet.avaliacao.dto.impl.AlunoDTO;
 import com.infnet.avaliacao.dto.impl.AvaliacaoDTO;
+import com.infnet.avaliacao.dto.impl.TemplateAvaliacaoDTO;
 import com.infnet.avaliacao.dto.impl.TurmaDTO;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +28,9 @@ public class AvaliacaoFacade implements IAvaliacaoFacade {
     @Resource
     private ITurmaService turmaService;
 
+    @Resource
+    private ITemplateAvaliacaoService templateAvaliacaoService;
+
     /**
      * {@inheritDoc}
      */
@@ -41,6 +46,9 @@ public class AvaliacaoFacade implements IAvaliacaoFacade {
                 AlunoDTO.toDto(this.alunoService.findByCpf(cpf)));
         avaliacaoDTO.setTurmaDTO(
                 TurmaDTO.toDto(this.turmaService.findById(idTurma)));
+        avaliacaoDTO.setTemplateAvaliacaoDTO(
+                TemplateAvaliacaoDTO.toDto(
+                        this.templateAvaliacaoService.findById(idTemplateAvaliacao)));
         return avaliacaoDTO;
     }
 
