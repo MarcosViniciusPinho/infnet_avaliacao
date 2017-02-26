513c841bb139e3e16b4ca947017fe5e481f1b73d
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-02-26 19:06:49 -0300

    Finalizando o salvar com todas as informações da avaliação(radio/texto)

--- src/main/resources/static/js/resposta/avaliacao/form.js
+++ src/main/resources/static/js/resposta/avaliacao/form.js
@@ -16,11 +16,26 @@ function addConteudoDaLista() {
     var totalPerguntas = $('#totalPerguntas').val();
     var checkeds = incializarOuRetornarMarcacao();
     for(var i = 1;i <= totalPerguntas; i++){
-        var inputName = "input[name='"+ i +"radio']:checked";
-        $(inputName).each(function (){
-            checkeds.push($(this).val());
-            $('#respostasSelecionadasComPerguntas').val(checkeds);
-        });
+        tratarTypeRadio(i, checkeds);
+        tratarTypeTextArea(i, checkeds);
+    }
+}
+
+function tratarTypeRadio(i, checkeds){
+    var inputName = "input[name='"+ i +"radio']:checked";
+    $(inputName).each(function (){
+        checkeds.push($(this).val());
+        $('#respostasSelecionadasComPerguntas').val(checkeds);
+    });
+}
+
+function tratarTypeTextArea(i, checkeds){
+    var valorResposta = $("#" + i + "textarea").val();
+    var idTemplatePergunta = $('#idTemplatePergunta').val();
+    if(valorResposta != undefined && idTemplatePergunta != undefined){
+        var respostaComPergunta = valorResposta + '-' + idTemplatePergunta;
+        checkeds.push(respostaComPergunta);
+        $('#respostasSelecionadasComPerguntas').val(checkeds);
     }
 }
 
