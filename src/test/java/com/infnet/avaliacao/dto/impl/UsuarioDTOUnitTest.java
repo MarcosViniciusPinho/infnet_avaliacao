0c4b6789c4bb2934b7625099c75f6ce7afd1ed08
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-07-02 15:01:23 -0300

    Adicionando mais testes de unidade.

---
+++ src/test/java/com/infnet/avaliacao/dto/impl/UsuarioDTOUnitTest.java
@@ -0,0 +1,63 @@
+package com.infnet.avaliacao.dto.impl;
+
+import com.infnet.avaliacao.entity.Perfil;
+import com.infnet.avaliacao.entity.Usuario;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsuarioDTOUnitTest {
+
+    @Test
+    public void testToEntity(){
+        Usuario usuarioEsperado = this.createUsuario(3L);
+        UsuarioDTO usuarioDTO = this.createUsuarioDTO(3L);
+        Assert.assertNotNull(usuarioDTO.toEntity());
+        Assert.assertEquals(usuarioEsperado, usuarioDTO.toEntity());
+    }
+
+    @Test
+    public void testToDto(){
+        Usuario usuario = this.createUsuario(3L);
+        UsuarioDTO usuarioDTOEsperado = this.createUsuarioDTO(3L);
+        Assert.assertNotNull(UsuarioDTO.toDto(usuario));
+        Assert.assertEquals(usuarioDTOEsperado, UsuarioDTO.toDto(usuario));
+    }
+
+    /**
+     * Métodos foram criados para auxiliar nos testes; ou seja; diminuir a codificação dos mesmos.
+     */
+
+    private Usuario createUsuario(Long id){
+        Perfil perfil = new Perfil();
+        perfil.setId(2L);
+        perfil.setNome("Perfil");
+
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNome("Marcos");
+        usuario.setLogin("123");
+        usuario.setSenha("Senha");
+        usuario.setPerfil(perfil);
+        return usuario;
+    }
+
+    private UsuarioDTO createUsuarioDTO(Long id){
+        Perfil perfil = new Perfil();
+        perfil.setId(2L);
+        perfil.setNome("Perfil");
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setId(id);
+        usuarioDTO.setNome("Marcos");
+        usuarioDTO.setLogin("123");
+        usuarioDTO.setSenha("Senha");
+        usuarioDTO.setPerfil(perfil);
+        return usuarioDTO;
+    }
+
+}
