6fc1c8775212fe298ade86ad9165aaa79e609ed8
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-07-02 12:28:49 -0300

    Adicionando mais testes de unidade.

--- src/test/java/com/infnet/avaliacao/exception/BusinessExceptionUnitTest.java
+++ src/test/java/com/infnet/avaliacao/exception/BusinessExceptionUnitTest.java
@@ -2,10 +2,15 @@ package com.infnet.avaliacao.exception;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BusinessExceptionUnitTest {
 
     @Test
