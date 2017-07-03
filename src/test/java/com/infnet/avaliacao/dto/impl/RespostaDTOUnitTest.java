9e86f3faae505fe5dc31a89f92c3d0fffa20c5b5
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-07-03 20:38:29 -0300

    Adicionando mais testes de unidade.

--- src/test/java/com/infnet/avaliacao/dto/impl/RespostaDTOUnitTest.java
+++ src/test/java/com/infnet/avaliacao/dto/impl/RespostaDTOUnitTest.java
@@ -7,17 +7,15 @@ import com.infnet.avaliacao.exception.NullParameterException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RespostaDTOUnitTest {
-
-    @Mock
-    private Pageable pageable;
 
     @Test
     public void testToEntity(){
@@ -40,6 +38,49 @@ public class RespostaDTOUnitTest {
         UsuarioDTO.toDto(null);
     }
 
+    @Test
+    public void testConvertListDtoToListEntity(){
+        List<Resposta> respostaList = new ArrayList<>();
+        respostaList.add(this.createResposta(2L));
+        respostaList.add(this.createResposta(6L));
+        respostaList.add(this.createResposta(4L));
+
+        List<RespostaDTO> respostaDTOList = new ArrayList<>();
+        respostaDTOList.add(this.createRespostaDTO(2L));
+        respostaDTOList.add(this.createRespostaDTO(6L));
+        respostaDTOList.add(this.createRespostaDTO(4L));
+
+        Assert.assertNotNull(RespostaDTO.convertListDtoToListEntity(respostaDTOList));
+        Assert.assertEquals(respostaList, RespostaDTO.convertListDtoToListEntity(respostaDTOList));
+    }
+
+    @Test
+    public void testConvertListDtoToListEntityEmpty(){
+        Assert.assertNotNull(RespostaDTO.convertListDtoToListEntity(new ArrayList<>()));
+        Assert.assertEquals(new ArrayList<Resposta>(), RespostaDTO.convertListDtoToListEntity(new ArrayList<>()));
+    }
+
+    @Test
+    public void testConvertListEntityToListDto(){
+        List<RespostaDTO> respostaDTOList = new ArrayList<>();
+        respostaDTOList.add(this.createRespostaDTO(2L));
+        respostaDTOList.add(this.createRespostaDTO(6L));
+        respostaDTOList.add(this.createRespostaDTO(4L));
+
+        List<Resposta> respostaList = new ArrayList<>();
+        respostaList.add(this.createResposta(2L));
+        respostaList.add(this.createResposta(6L));
+        respostaList.add(this.createResposta(4L));
+
+        Assert.assertNotNull(RespostaDTO.convertListEntityToListDto(respostaList));
+        Assert.assertEquals(respostaDTOList, RespostaDTO.convertListEntityToListDto(respostaList));
+    }
+
+    @Test
+    public void testConvertListEntityToListDtoEmpty(){
+        Assert.assertNotNull(RespostaDTO.convertListEntityToListDto(new ArrayList<>()));
+        Assert.assertEquals(new ArrayList<RespostaDTO>(), RespostaDTO.convertListEntityToListDto(new ArrayList<>()));
+    }
 
     /**
      * Métodos foram criados para auxiliar nos testes; ou seja; diminuir a codificação dos mesmos.
