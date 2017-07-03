60e72057f5b47dc64dee6c22fc71f9afad4b4c29
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-07-03 20:09:53 -0300

    Adicionando mais testes de unidade.

--- src/test/java/com/infnet/avaliacao/dto/impl/TemplateTopicoDTOUnitTest.java
+++ src/test/java/com/infnet/avaliacao/dto/impl/TemplateTopicoDTOUnitTest.java
@@ -8,7 +8,11 @@ import com.infnet.avaliacao.exception.NullParameterException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -17,6 +21,9 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TemplateTopicoDTOUnitTest {
+
+    @Mock
+    private Pageable pageable;
 
     @Test
     public void testToEntity(){
@@ -37,6 +44,78 @@ public class TemplateTopicoDTOUnitTest {
     @Test(expected = NullParameterException.class)
     public void testToDtoFailedAlunoNull(){
         TemplateTopicoDTO.toDto(null);
+    }
+
+    @Test
+    public void testConvertListEntityToListDto(){
+        List<TemplateTopicoDTO> templateTopicoDTOList = new ArrayList<>();
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(2L));
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(6L));
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(4L));
+
+        List<TemplateTopico> templateTopicoList = new ArrayList<>();
+        templateTopicoList.add(this.createTemplateTopico(2L));
+        templateTopicoList.add(this.createTemplateTopico(6L));
+        templateTopicoList.add(this.createTemplateTopico(4L));
+
+        Assert.assertNotNull(TemplateTopicoDTO.convertListEntityToListDto(templateTopicoList));
+        Assert.assertEquals(templateTopicoDTOList, TemplateTopicoDTO.convertListEntityToListDto(templateTopicoList));
+    }
+
+    @Test
+    public void testConvertListEntityToListDtoEmpty(){
+        Assert.assertNotNull(TemplateTopicoDTO.convertListEntityToListDto(new ArrayList<>()));
+        Assert.assertEquals(new ArrayList<TemplateTopicoDTO>(), TemplateTopicoDTO.convertListEntityToListDto(new ArrayList<>()));
+    }
+
+    @Test
+    public void testConvertListDtoToListEntity(){
+        List<TemplateTopico> templateTopicoList = new ArrayList<>();
+        templateTopicoList.add(this.createTemplateTopico(2L));
+        templateTopicoList.add(this.createTemplateTopico(6L));
+        templateTopicoList.add(this.createTemplateTopico(4L));
+
+        List<TemplateTopicoDTO> templateTopicoDTOList = new ArrayList<>();
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(2L));
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(6L));
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(4L));
+
+        Assert.assertNotNull(TemplateTopicoDTO.convertListDtoToListEntity(templateTopicoDTOList));
+        Assert.assertEquals(templateTopicoList, TemplateTopicoDTO.convertListDtoToListEntity(templateTopicoDTOList));
+    }
+
+    @Test
+    public void testConvertListDtoToListEntityEmpty(){
+        Assert.assertNotNull(TemplateTopicoDTO.convertListDtoToListEntity(new ArrayList<>()));
+        Assert.assertEquals(new ArrayList<TemplateTopico>(), TemplateTopicoDTO.convertListDtoToListEntity(new ArrayList<>()));
+    }
+
+    @Test
+    public void testConvertPageEntityToPageDto(){
+        List<TemplateTopicoDTO> templateTopicoDTOList = new ArrayList<>();
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(7L));
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(2L));
+        templateTopicoDTOList.add(this.createTemplateTopicoDTO(5L));
+
+        Page<TemplateTopicoDTO> pageDtoList = new PageImpl<>(templateTopicoDTOList, pageable, templateTopicoDTOList.size());
+
+        List<TemplateTopico> templateTopicoList = new ArrayList<>();
+        templateTopicoList.add(this.createTemplateTopico(7L));
+        templateTopicoList.add(this.createTemplateTopico(2L));
+        templateTopicoList.add(this.createTemplateTopico(5L));
+
+        Page<TemplateTopico> pageList = new PageImpl<>(templateTopicoList, pageable, templateTopicoList.size());
+
+        Assert.assertNotNull(TemplateTopicoDTO.convertPageEntityToPageDto(pageList, this.pageable));
+        Assert.assertEquals(pageDtoList, TemplateTopicoDTO.convertPageEntityToPageDto(pageList, this.pageable));
+    }
+
+    @Test
+    public void testConvertPageEntityToPageDtoEmpty(){
+        Page<TemplateTopicoDTO> pageDtoList = new PageImpl<>(new ArrayList<>(), pageable, 0);
+        Page<TemplateTopico> pageList = new PageImpl<>(new ArrayList<>(), pageable, 0);
+        Assert.assertNotNull(TemplateTopicoDTO.convertPageEntityToPageDto(pageList, this.pageable));
+        Assert.assertEquals(pageDtoList, TemplateTopicoDTO.convertPageEntityToPageDto(pageList, this.pageable));
     }
 
     /**
