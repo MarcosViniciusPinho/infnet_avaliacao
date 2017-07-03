8c9fff503c95daee5f1e963aea1ba8d461cb9aa8
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-07-03 20:59:57 -0300

    Adicionando mais testes de unidade.

--- src/test/java/com/infnet/avaliacao/dto/impl/TemplateAvaliacaoTopicoPerguntaDTOUnitTest.java
+++ src/test/java/com/infnet/avaliacao/dto/impl/TemplateAvaliacaoTopicoPerguntaDTOUnitTest.java
@@ -11,6 +11,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TemplateAvaliacaoTopicoPerguntaDTOUnitTest {
@@ -32,8 +35,52 @@ public class TemplateAvaliacaoTopicoPerguntaDTOUnitTest {
     }
 
     @Test(expected = NullParameterException.class)
-    public void testToDtoFailedAlunoNull(){
+    public void testToDtoFailedTemplateAvaliacaoTopicoPerguntaNull(){
         TemplateAvaliacaoTopicoPerguntaDTO.toDto(null);
+    }
+
+    @Test
+    public void testConvertListEntityToListDto(){
+        List<TemplateAvaliacaoTopicoPerguntaDTO> templateAvaliacaoTopicoPerguntaDTOList = new ArrayList<>();
+        templateAvaliacaoTopicoPerguntaDTOList.add(this.createTemplateAvaliacaoTopicoPerguntaDTO(2L));
+        templateAvaliacaoTopicoPerguntaDTOList.add(this.createTemplateAvaliacaoTopicoPerguntaDTO(6L));
+        templateAvaliacaoTopicoPerguntaDTOList.add(this.createTemplateAvaliacaoTopicoPerguntaDTO(4L));
+
+        List<TemplateAvaliacaoTopicoPergunta> templateAvaliacaoTopicoPerguntaList = new ArrayList<>();
+        templateAvaliacaoTopicoPerguntaList.add(this.createTemplateAvaliacaoTopicoPergunta(2L));
+        templateAvaliacaoTopicoPerguntaList.add(this.createTemplateAvaliacaoTopicoPergunta(6L));
+        templateAvaliacaoTopicoPerguntaList.add(this.createTemplateAvaliacaoTopicoPergunta(4L));
+
+        Assert.assertNotNull(TemplateAvaliacaoTopicoPerguntaDTO.convertListEntityToListDto(templateAvaliacaoTopicoPerguntaList));
+        Assert.assertEquals(templateAvaliacaoTopicoPerguntaDTOList, TemplateAvaliacaoTopicoPerguntaDTO.convertListEntityToListDto(templateAvaliacaoTopicoPerguntaList));
+    }
+
+    @Test
+    public void testConvertListEntityToListDtoEmpty(){
+        Assert.assertNotNull(TemplateAvaliacaoTopicoPerguntaDTO.convertListEntityToListDto(new ArrayList<>()));
+        Assert.assertEquals(new ArrayList<TemplateAvaliacaoTopicoPerguntaDTO>(), TemplateAvaliacaoTopicoPerguntaDTO.convertListEntityToListDto(new ArrayList<>()));
+    }
+
+    @Test
+    public void testConvertListDtoToListEntity(){
+        List<TemplateAvaliacaoTopicoPergunta> templateAvaliacaoTopicoPerguntaList = new ArrayList<>();
+        templateAvaliacaoTopicoPerguntaList.add(this.createTemplateAvaliacaoTopicoPergunta(2L));
+        templateAvaliacaoTopicoPerguntaList.add(this.createTemplateAvaliacaoTopicoPergunta(6L));
+        templateAvaliacaoTopicoPerguntaList.add(this.createTemplateAvaliacaoTopicoPergunta(4L));
+
+        List<TemplateAvaliacaoTopicoPerguntaDTO> templateAvaliacaoTopicoPerguntaDTOList = new ArrayList<>();
+        templateAvaliacaoTopicoPerguntaDTOList.add(this.createTemplateAvaliacaoTopicoPerguntaDTO(2L));
+        templateAvaliacaoTopicoPerguntaDTOList.add(this.createTemplateAvaliacaoTopicoPerguntaDTO(6L));
+        templateAvaliacaoTopicoPerguntaDTOList.add(this.createTemplateAvaliacaoTopicoPerguntaDTO(4L));
+
+        Assert.assertNotNull(TemplateAvaliacaoTopicoPerguntaDTO.convertListDtoToListEntity(templateAvaliacaoTopicoPerguntaDTOList));
+        Assert.assertEquals(templateAvaliacaoTopicoPerguntaList, TemplateAvaliacaoTopicoPerguntaDTO.convertListDtoToListEntity(templateAvaliacaoTopicoPerguntaDTOList));
+    }
+
+    @Test
+    public void testConvertListDtoToListEntityEmpty(){
+        Assert.assertNotNull(TemplateAvaliacaoTopicoPerguntaDTO.convertListDtoToListEntity(new ArrayList<>()));
+        Assert.assertEquals(new ArrayList<TemplateAvaliacaoTopicoPergunta>(), TemplateAvaliacaoTopicoPerguntaDTO.convertListDtoToListEntity(new ArrayList<>()));
     }
 
     /**
